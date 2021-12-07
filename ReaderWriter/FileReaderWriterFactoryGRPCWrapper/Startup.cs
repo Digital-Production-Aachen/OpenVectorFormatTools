6e461ff7eecdfbba1c50539fdb104bd2dddf8ba3
Alexander Meyer <alexander.meyer@ilt.fraunhofer.de>
2021-12-07 17:57:54 +0100

    Added new, forgotten files to gRPCWrapper

---
+++ ReaderWriter/FileReaderWriterFactoryGRPCWrapper/Startup.cs
@@ -0,0 +1,55 @@
+﻿/*
+---- Copyright Start ----
+
+This file is part of the OpenVectorFormatTools collection. This collection provides tools to facilitate the usage of the OpenVectorFormat.
+
+Copyright (C) 2021 Digital-Production-Aachen
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+---- Copyright End ----
+*/
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace OpenVectorFormat.FileHandlerFactoryGRPCWrapper
+{
+    class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+            services.AddSingleton(new GRPCWrapperFunctionsImplementation());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GRPCWrapperFunctionsImplementation>();
+            });
+        }
+    }
+}
