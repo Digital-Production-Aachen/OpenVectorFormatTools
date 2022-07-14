64c6f7bd62f5b0132669cf0c1885b6c0baf59c75
Sebastian Dirks <sebastian.dirks@dap.rwth-aachen.de>
2022-07-14 16:04:17 +0200

    Added locks to readHelper to make it thread safe. Removed depricated code.

--- ReaderWriter/3rdPartyFormatAdapters/CLI_ILT/iltFileReader/Controller/readHelper.cs
+++ ReaderWriter/3rdPartyFormatAdapters/CLI_ILT/iltFileReader/Controller/readHelper.cs
@@ -25,49 +25,22 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace OpenVectorFormat.ILTFileReader.Controller
 {
     class readHelper
     {
-
-        public static UInt16 readUint16(StreamReader reader, ref long index) {
-            byte[] bytes = new byte[2];
-            bytes[0] = readByte(reader, ref index);
-            bytes[1] = readByte(reader, ref index);
-            return BitConverter.ToUInt16(bytes,0);
-        }
-
-        public static int readInt(StreamReader reader, ref long index)
-        {
-            byte[] bytes = new byte[4];
-            bytes[0] = readByte(reader, ref index);
-            bytes[1] = readByte(reader, ref index);
-            bytes[2] = readByte(reader, ref index);
-            bytes[3] = readByte(reader, ref index);
-            return BitConverter.ToInt32(bytes, 0);
-        }
-
-        public static float readReal(StreamReader reader, ref long index) {
-
-            byte[] bytes = new byte[4];
-            bytes[0] = readByte(reader, ref index);
-            bytes[1] = readByte(reader, ref index);
-            bytes[2] = readByte(reader, ref index);
-            bytes[3] = readByte(reader, ref index);
-            return BitConverter.ToSingle(bytes, 0); //Good Vartype name for computer scientists
-        }
-
         public static float[] readFloatArray(BinaryReader reader, long loc, int length) {
             List<float> ret = new List<float>();
-
-            if (reader.BaseStream.Position != loc)     //resync our filereader in case we've jumped vectorcoords or stuff, we don't want to have in ram
-                reader.BaseStream.Seek(loc, SeekOrigin.Begin);
-
-            while (reader.BaseStream.Position < length + loc) //save the location to prevent endless loop, because offset get's incremented, while loc shouldn't
+            lock (reader.BaseStream)
             {
-                ret.Add(reader.ReadSingle()); //Good Vartype name for computer scientists
+                if (reader.BaseStream.Position != loc)     //resync our filereader in case we've jumped vectorcoords or stuff, we don't want to have in ram
+                    reader.BaseStream.Seek(loc, SeekOrigin.Begin);
+
+                while (reader.BaseStream.Position < length + loc) //save the location to prevent endless loop, because offset get's incremented, while loc shouldn't
+                {
+                    ret.Add(reader.ReadSingle()); //Good Vartype name for computer scientists
+                }
             }
             return ret.ToArray();
         }
@@ -75,29 +48,17 @@ namespace OpenVectorFormat.ILTFileReader.Controller
         public static float[] readFloatArrayFromUshorts(BinaryReader reader, long loc, int length)
         {
             List<float> ret = new List<float>();
-            if (reader.BaseStream.Position != loc)     //resync our filereader in case we've jumped vectorcoords or stuff, we don't want to have in ram
-                reader.BaseStream.Seek(loc, SeekOrigin.Begin);
-
-            while (reader.BaseStream.Position < length + loc)
+            lock (reader.BaseStream)
             {
-                ret.Add(Convert.ToSingle(reader.ReadUInt16())); //Good Vartype name for computer scientists
+                if (reader.BaseStream.Position != loc)     //resync our filereader in case we've jumped vectorcoords or stuff, we don't want to have in ram
+                    reader.BaseStream.Seek(loc, SeekOrigin.Begin);
+
+                while (reader.BaseStream.Position < length + loc)
+                {
+                    ret.Add(Convert.ToSingle(reader.ReadUInt16())); //Good Vartype name for computer scientists
+                }
             }
             return ret.ToArray();
-        }
-
-        private static byte readByte(StreamReader reader, ref long index) {
-            byte beyti;
-            char[] charr = new char[1];
-            if(reader.BaseStream.Position != index)     //resync our filereader in case we've jumped vectorcoords or stuff, we don't want to have in ram
-                reader.BaseStream.Seek(index, SeekOrigin.Begin);
-            beyti = (byte)reader.BaseStream.ReadByte(); //ReadByte returns int but! byte casted to int or -1 if EndOfStream/-File
-            //reader.Read(charr, Convert.ToInt32(index), 1);
-            //reader.BaseStream = new BufferedStream(reader.BaseStream);
-            //reader = new BinaryReader(new BufferedStream(reader.BaseStream));
-            //beyti = (byte)reader.Read();
-            index++;
-            return beyti;
-            //return (byte)charr[0];
         }
     }
 }
