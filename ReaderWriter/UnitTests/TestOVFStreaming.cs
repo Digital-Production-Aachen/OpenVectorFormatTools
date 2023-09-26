96a5c07241a52b462db5caa444a7c82d74f8f359
Max Fröhlich <max.froehlich@dap.rwth-aachen.de>
2023-09-26 16:07:10 +0200

    added test class and test ovf files

---
+++ ReaderWriter/UnitTests/TestOVFStreaming.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestOVFStreaming
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
