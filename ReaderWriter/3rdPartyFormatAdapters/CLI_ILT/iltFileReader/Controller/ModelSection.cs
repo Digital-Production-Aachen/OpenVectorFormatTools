6760d5f113c8e19a94bd5bc147435608925be38d
Max Fröhlich <Max.Froehlich@dap.rwth-aachen.de>
2024-07-08 16:58:18 +0200

    Added netfabb ilt test file. WIP: regex for modelsection that matches the netfabb "naming convention"

--- ReaderWriter/3rdPartyFormatAdapters/CLI_ILT/iltFileReader/Controller/ModelSection.cs
+++ ReaderWriter/3rdPartyFormatAdapters/CLI_ILT/iltFileReader/Controller/ModelSection.cs
@@ -48,9 +48,89 @@ namespace OpenVectorFormat.ILTFileReader.Controller
         }
 
 
+        private bool MatchMagics(string fileName)
+        {
+            var result = Regex.Match(fileName, @"(?<name>[a-z]+_[0-9]+)_(?<part_area>..?)_(?<vector_class>..+)\.cli$");
+            if (!result.Success) return false;
+            ModelsectionName = result.Groups["name"].Value;
+            switch (result.Groups["part_area"].Value)
+            {
+                case "st":
+                    this.Type = PartArea.st;
+                    break;
+                case "k":
+                    this.Type = PartArea.k;
+                    break;
+                case "s1":
+                    this.Type = PartArea.s1;
+                    break;
+                case "s2":
+                    this.Type = PartArea.s2;
+                    break;
+                default:
+                    this.Type = PartArea.s2;
+                    break;
+            }
+            switch (result.Groups["vector_class"].Value)
+            {
+                case "vs":
+                    this.SubType = VectorClass.vs;
+                    break;
+                case "vk":
+                    this.SubType = VectorClass.vk;
+                    break;
+                case "us":
+                    this.SubType = VectorClass.us;
+                    break;
+                case "uk":
+                    this.SubType = VectorClass.uk;
+                    break;
+                case "kv":
+                    this.SubType = VectorClass.kv;
+                    break;
+                case "sx":
+                    this.SubType = VectorClass.sx;
+                    break;
+                case "kvu":
+                    this.SubType = VectorClass.kvu;
+                    break;
+                case "skin":
+                    this.SubType = VectorClass.skin;
+                    break;
+                default:
+                    this.SubType = VectorClass.vs;
+                    break;
+            }
+            return true;
+        }
+
+        private bool MatchNetfabb(string fileName)
+        {
+            //throw new NotImplementedException();
+            var result = Regex.Match(fileName, @"(?<name>\S+)\s*(?<support>\(\S+\))?\s*(?<filling>\(\S+\))?\.cli$");
+            if (!result.Success) return false;
+            ModelsectionName = result.Groups["name"].Value;
+            switch (result.Groups["support"].Value)
+            {
+                case "support":
+                    break;
+                case "solidsupport":
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+
+
         private void ParseFileName()
         {
+            // ([a-z]+_[0-9]+)_(..?)_(..+)\.cli$
             string fileName = Path.GetFileName(this.cliFileName);
+            if (MatchMagics(fileName)) return;
+            if (MatchNetfabb(fileName)) return;
+            throw new Exception();
+#if false
             ModelsectionName = Regex.Match(fileName, @"([a-z]+_[0-9]+)_.+_.+\.cli$").Groups[1].Value;
             String partArea = Regex.Match(fileName, @"_(..?)_(..+)\.cli$").Groups[1].Value;
             switch (partArea)
@@ -103,6 +183,10 @@ namespace OpenVectorFormat.ILTFileReader.Controller
                     this.SubType = VectorClass.vs;
                     break;
             }
+#endif
+
+            // netfabb regex: (\S+)\s*(\(\S+\))?\s*(\(\S+\))?\.cli
+            // part name, support type, filling type
 
         }
 
