16bf47258a13d881cf8aede526ce2c0fd1d800a2
Sven Erb <sven.erb@dap.rwth-aachen.de>
2025-03-18 11:59:05 +0100

    Modified test file.

--- ReaderWriter/UnitTests/GCodeTest.cs
+++ ReaderWriter/UnitTests/GCodeTest.cs
@@ -78,8 +78,8 @@ namespace OpenVectorFormat.ReaderWriter.UnitTests
             string[] testCommands = File.ReadAllLines(fileName.FullName);
 
             GCodeCommandList gCodeCommandList = new GCodeCommandList(testCommands);
-            Assert.AreEqual(19335, gCodeCommandList.OfType<LinearInterpolationCmd>().ToList().Count);
-            Assert.AreEqual(185, gCodeCommandList.OfType<MiscCommand>().ToList().Count);
+            Assert.AreEqual(19331, gCodeCommandList.OfType<LinearInterpolationCmd>().ToList().Count);
+            Assert.AreEqual(183, gCodeCommandList.OfType<MiscCommand>().ToList().Count);
         }
 
         [DynamicData("GCodeFiles")]
