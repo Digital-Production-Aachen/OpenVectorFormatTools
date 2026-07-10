c83978f8fe184e4337c5d285221a0b76cfbff67f
Sven Erb <sven.erb@dap.rwth-aachen.de>
2026-07-10 16:25:11 +0200

    Updated writer logic to align with reader logic.

--- ReaderWriter/3rdPartyFormatAdapters/GCODE/GCodeReaderWriter/GCodeWriter.cs
+++ ReaderWriter/3rdPartyFormatAdapters/GCODE/GCodeReaderWriter/GCodeWriter.cs
@@ -23,11 +23,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 using GCodeReaderWriter.Commands;
-using OpenVectorFormat;
 using OpenVectorFormat.AbstractReaderWriter;
 using OpenVectorFormat.OVFReaderWriter;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -40,27 +40,24 @@ namespace OpenVectorFormat.GCodeReaderWriter
         private IFileReaderWriterProgress _progress;
         private string _filename;
         private StreamWriter _fs;
-
-        public override Job JobShell { get { return _jobShell; } }
         private Job _jobShell;
-        float[] _lastPt = null;
-        float _currentZ;
+        private FileWriteOperation _fileOperationInProgress = FileWriteOperation.None;
 
-        private readonly OvfToGCodeCommandFactory _factory;
-
-        public GCodeWriter() : this(new OvfToGCodeCommandFactory()) { }
-
-        public GCodeWriter(OvfToGCodeCommandFactory factory)
-        {
-            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
-        }
+        /// <inheritdoc/>
+        public override Job JobShell { get { return _jobShell; } }
 
         /// <inheritdoc/>
         public override FileWriteOperation FileOperationInProgress { get { return _fileOperationInProgress; } }
-        private FileWriteOperation _fileOperationInProgress = FileWriteOperation.None;
 
-        private MarkingParams _lastWrittenParams = null;
+        // Current writer states.
+        float[] _lastPosition = null;
+        float _currentZ;
+        float? _lastX;
+        float? _lastY;
 
+        /// <summary>
+        /// Processes the OVF file and writes the corresponding GCode to the specified output path.
+        /// </summary>
         public void ProcessOVFtoGCode(OVFFileReader ovfReader, string gcodeOutputPath)
         {
             Job job = ovfReader.CacheJobToMemory();
@@ -73,6 +70,7 @@ namespace OpenVectorFormat.GCodeReaderWriter
             this.SimpleJobWrite(job, gcodeOutputPath);
         }
 
+        /// <inheritdoc/>
         public override void AppendWorkPlane(WorkPlane workPlane)
         {
             _currentZ = workPlane.ZPosInMm;
@@ -85,22 +83,25 @@ namespace OpenVectorFormat.GCodeReaderWriter
 
                     bool isFirstBlockInPlane = (repeatIndex == 0 && blockIndex == 0);
 
+                    // Inject a Z move before the first block in a workplane if the block translates to a movement.
                     bool injectZ = isFirstBlockInPlane &&
                            (block.VectorDataCase == VectorBlock.VectorDataOneofCase.PointSequence ||
                             block.VectorDataCase == VectorBlock.VectorDataOneofCase.Hatches ||
                             block.VectorDataCase == VectorBlock.VectorDataOneofCase.Arcs ||
                             block.VectorDataCase == VectorBlock.VectorDataOneofCase.LineSequence);
 
-                    AddVectorBlock(block, injectZ);
+                    HandleVectorBlock(block, injectZ);
                 }
             }
         }
 
+        /// <inheritdoc/>
         public override void AppendVectorBlock(VectorBlock block)
         {
-            AddVectorBlock(block, false);
+            HandleVectorBlock(block, false);
         }
 
+        /// <inheritdoc/>
         public override void Dispose()
         {
             if (_fileOperationInProgress != FileWriteOperation.None)
@@ -109,38 +110,178 @@ namespace OpenVectorFormat.GCodeReaderWriter
             }
         }
 
-        private void AddVectorBlock(VectorBlock block, bool injectZ)
+        private void HandleVectorBlock(VectorBlock block, bool injectZ)
         {
-            var ctx = new GCodeWriterContext(_currentZ, injectZ);
+            // Save the previous point before each block, so that the first move of a new block can be compared
+            // against the last point of the previous block.
+            _lastX = _lastPosition?[0];
+            _lastY = _lastPosition?[1];
+
             for (ulong i = 0; i < block.Repeats + 1; i++)
             {
-                foreach (var cmd in _factory.CreateCommands(block, ctx))
-                {
-                    if (cmd is LinearInterpolationCmd linCmd)
-                    {
-                        float[] target = MoveTarget(linCmd);
-                        if (!linCmd.isOperation)
-                        {
-                            if (_lastPt != null && target.SequenceEqual(_lastPt))
-                                continue;
-                        }
-                        _lastPt = target;
-                    }
-                    _fs.WriteLine(cmd.ToString());
-                }
+                foreach (var cmd in WriteVectorBlockData(block, injectZ))
+                    Write(cmd);
             }
         }
 
-        private static float[] MoveTarget(LinearInterpolationCmd cmd)
+        private void Write(GCodeCommand cmd)
         {
-            return cmd.zPosition.HasValue
-                ? new[] { cmd.xPosition.Value, cmd.yPosition.Value, cmd.zPosition.Value }
-                : new[] { cmd.xPosition.Value, cmd.yPosition.Value };
+            // Make sure a travel move whose X/Y equals the last point is skipped.
+            // Z-only moves have no X/Y component and are always written.
+            if (cmd is LinearInterpolationCmd linCmd &&
+                (linCmd.xPosition.HasValue || linCmd.yPosition.HasValue))
+            {
+                float[] target = { linCmd.xPosition.Value, linCmd.yPosition.Value };
+                if (!linCmd.isOperation && _lastPosition != null && target.SequenceEqual(_lastPosition))
+                    return;
+                _lastPosition = target;
+            }
+            _fs.WriteLine(cmd.ToString());
         }
 
+        // Command handlers translate OVF vector data into GCodeCommands.
+        // Override these (or the small move helpers below) in a subclass to change the interpretation.
+        protected virtual IEnumerable<GCodeCommand> WriteVectorBlockData(VectorBlock block, bool injectZ)
+        {
+            switch (block.VectorDataCase)
+            {
+                case VectorBlock.VectorDataOneofCase.PointSequence:
+                    return WritePointSequence(block, injectZ);
+                case VectorBlock.VectorDataOneofCase.Hatches:
+                    return WriteHatches(block, injectZ);
+                case VectorBlock.VectorDataOneofCase.Arcs:
+                    return WriteArcs(block, injectZ);
+                case VectorBlock.VectorDataOneofCase.ExposurePause:
+                    return WriteExposurePause(block, injectZ);
+                case VectorBlock.VectorDataOneofCase.LineSequence:
+                    return WriteLineSequence(block, injectZ);
+                default:
+                    throw new NotImplementedException($"VectorDataCase {block.VectorDataCase} is not supported.");
+            }
+        }
+
+        /// <summary>
+        /// Writes point sequence data as a series of travel moves.
+        /// </summary>
+        protected virtual IEnumerable<GCodeCommand> WritePointSequence(VectorBlock block, bool injectZ)
+        {
+            var pts = block.PointSequence.Points;
+            for (int i = 0; i < pts.Count; i += 2)
+            {
+                if (injectZ && i == 0)
+                    yield return InjectedTravel(pts[i], pts[i + 1]);
+                else
+                    yield return TravelMove(pts[i], pts[i + 1]);
+            }
+        }
+
+        /// <summary>
+        /// Writes hatch data as a series of moves.
+        /// </summary>
+        protected virtual IEnumerable<GCodeCommand> WriteHatches(VectorBlock block, bool injectZ)
+        {
+            var pts = block.Hatches.Points;
+            for (int i = 0; i < pts.Count; i += 4)
+            {
+                if (injectZ && i == 0)
+                    yield return InjectedTravel(pts[i], pts[i + 1]);
+                else
+                    yield return TravelMove(pts[i], pts[i + 1]);
+
+                yield return FeedMove(pts[i + 2], pts[i + 3]);
+            }
+        }
+
+        /// <summary>
+        /// Writes arc data as a series of circular interpolation moves.
+        /// </summary>
+        protected virtual IEnumerable<GCodeCommand> WriteArcs(VectorBlock block, bool injectZ)
+        {
+            double angle = block.Arcs.Angle;
+            float startX = block.Arcs.StartDx;
+            float startY = block.Arcs.StartDy;
+            var centers = block.Arcs.Centers;
+
+            // If the first move in a workplane is an arc,
+            // inject a Z move to the workplane height before the arc.
+            if (injectZ)
+                yield return ZMove(_currentZ);
+
+            for (int i = 0; i < centers.Count; i += 2)
+            {
+                float cx = centers[i], cy = centers[i + 1];
+                double iOffset = cx - startX;
+                double jOffset = cy - startY;
+                double deltaX = startX - cx, deltaY = startY - cy;
+                double radius = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+                double angleStart = Math.Atan2(deltaY, deltaX);
+                double angleFinal = NormalizeAngle(angleStart - angle);
+                double endX = cx + radius * Math.Cos(angleFinal);
+                double endY = cy + radius * Math.Sin(angleFinal);
+
+                if (angle > 0)
+                    yield return new CircularInterpolationCmd(PrepCode.G, 2, true,
+                        (float)endX, (float)endY, (float)iOffset, (float)jOffset, null, null);
+                else if (angle < 0)
+                    yield return new CircularInterpolationCmd(PrepCode.G, 3, false,
+                        (float)endX, (float)endY, (float)iOffset, (float)jOffset, null, null);
+                else
+                    throw new ArgumentException("Arc angle must be non-zero.");
+            }
+        }
+
+        /// <summary>
+        /// Writes an exposure pause command as a G4 command.
+        /// </summary>
+        protected virtual IEnumerable<GCodeCommand> WriteExposurePause(VectorBlock block, bool injectZ)
+        {
+            yield return new PauseCommand(PrepCode.G, 4, (int)block.ExposurePause.PauseInUs);
+        }
+
+        /// <summary>
+        /// Writes line sequence data as a series of moves.
+        /// </summary>
+        protected virtual IEnumerable<GCodeCommand> WriteLineSequence(VectorBlock block, bool injectZ)
+        {
+            var pts = block.LineSequence.Points;
+            if (injectZ)
+                yield return InjectedTravel(pts[0], pts[1]);
+            else
+                yield return TravelMove(pts[0], pts[1]);
+
+            for (int i = 2; i < pts.Count; i += 2)
+                yield return FeedMove(pts[i], pts[i + 1]);
+        }
+
+        /// <summary>
+        /// First travel move of a workplane: writes a standalone Z move when X/Y is
+        /// unchanged from the previous point, otherwise write a combined X/Y/Z travel move.
+        /// </summary>
+        protected virtual LinearInterpolationCmd InjectedTravel(float x, float y)
+            => (_lastX == x && _lastY == y)
+                ? ZMove(_currentZ)
+                : TravelMove(x, y, _currentZ);
+
+        protected virtual LinearInterpolationCmd ZMove(float z)
+            => new LinearInterpolationCmd(PrepCode.G, 0, false, null, null, z);
+
+        protected virtual LinearInterpolationCmd TravelMove(float x, float y, float? z = null)
+            => new LinearInterpolationCmd(PrepCode.G, 0, false, x, y, z);
+
+        protected virtual LinearInterpolationCmd FeedMove(float x, float y, float? z = null)
+            => new LinearInterpolationCmd(PrepCode.G, 1, true, x, y, z);
+
+        private static double NormalizeAngle(double angle)
+        {
+            angle %= 2 * Math.PI;
+            if (angle > Math.PI) angle -= 2 * Math.PI;
+            else if (angle <= -Math.PI) angle += 2 * Math.PI;
+            return angle;
+        }
+
+        /// <inheritdoc/>
         public override void SimpleJobWrite(Job job, string filename, IFileReaderWriterProgress progress = null)
         {
-            CheckConsistence(job.NumWorkPlanes, job.WorkPlanes.Count);
             for (int i = 0; i < job.NumWorkPlanes; i++)
             {
                 int expected = job.WorkPlanes[i].NumBlocks;
@@ -163,6 +304,8 @@ namespace OpenVectorFormat.GCodeReaderWriter
             _fs.Close();
             _fileOperationInProgress = FileWriteOperation.None;
         }
+
+        /// <inheritdoc/>
         public override void StartWritePartial(Job jobShell, string filename, IFileReaderWriterProgress progress = null)
         {
             _jobShell = jobShell;
@@ -170,14 +313,6 @@ namespace OpenVectorFormat.GCodeReaderWriter
             _fs = new StreamWriter(filename);
             this._filename = filename;
             this._progress = progress;
-        }
-        private void CheckConsistence(int number1, int number2)
-        {
-            if (number1 != number2)
-            {
-                Dispose();
-                throw new IOException("inconsistence in file detected");
-            }
         }
     }
 }
